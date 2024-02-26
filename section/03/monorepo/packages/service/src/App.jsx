9d9be2b0eede5e44ed701415d25e1dd02553ca5f
하정훈 <hajh1994@gmail.com>
2024-02-26 22:09:44 +0900

    chore: add section 03

---
+++ section/03/monorepo/packages/service/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import logo from './logo.svg'
+import { add } from '@hajeonghun/mono-math'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>My Custom React App.(JavaScript)</h1>
+        <h2>{add(3, 10)}</h2>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
---
+++ section/03/polyrepo/math/library/index.js
@@ -0,0 +1,7 @@
+export function add (a, b) {
+    return a + b + 300;
+}
+
+export function multiply (a, b) {
+    return a * b;
+}
